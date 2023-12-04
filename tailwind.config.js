51da02224ef193f53ce77a98ee7cb0e4175a7839
kuryart <kuryart1@gmail.com>
2023-12-04 23:10:42 +0000

    define some extra small breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     colors: false,
+    screens: {
+      "2xs": "280px",
+      xs: "320px",
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',     
+    }
   },
   plugins: [require('@tailwindcss/typography'), require("daisyui")],
   daisyui: {
