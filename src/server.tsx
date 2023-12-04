9f81f84cc73646b7d199f754b54358c941c9a69a
kuryart <kuryart1@gmail.com>
2023-12-04 23:17:49 +0000

    configure server routes and localization

--- src/server.tsx
+++ src/server.tsx
@@ -1,7 +1,11 @@
-import { MusicPage } from "./components/MusicPage";
-import { CurrentTime } from "./components/CurrentTime";
-import { HomePage } from "./components/HomePage";
 import { html, serve_static } from "./response";
+import { HomePage } from "./components/HomePage";
+import { MusicPage } from "./components/MusicPage";
+import { GamePage } from "./components/GamePage";
+import { VisualArtPage } from "./components/VisualArtPage";
+
+// import { t, getLanguage } from "./localization";
+import { isLanguageValid, setLocalization } from "./localization";
 
 // Configuration
 const port = process.env?.PORT ? Number(process.env.PORT) : 3000;
@@ -17,11 +21,29 @@ export function start() {
       console.log(`[request]: ${req.method}: ${req.url}`);
 
       const url = new URL(req.url);
-
-      // Handle routes
+      // - Handle routes
+      // -- Main routes
       if (url.pathname === "/") return html(<HomePage />);
-      if (url.pathname === "/about") return html(<MusicPage />);
-      if (url.pathname === "/time") return html(<CurrentTime />);
+      if (url.pathname === "/music") return html(<MusicPage />);
+      if (url.pathname === "/game") return html(<GamePage />);
+      if (url.pathname === "/visual-art") return html(<VisualArtPage />);
+
+      // -- Localization
+      const paths = url.pathname.split(/[\/]+/);
+
+      if (isLanguageValid(paths[1])) {
+        const lang = paths[1];
+        const path = "/" + paths.slice(2).join("/");
+
+        setLocalization(lang, path);
+
+        if (path === "/") return html(<HomePage />);
+        if (path === "/music") return html(<MusicPage />);
+        if (path === "/game") return html(<GamePage />);
+        if (path === "/visual-art") return html(<VisualArtPage />);
+
+        // console.log(`[localization]: ${lang} ${path}`);
+      }
 
       // Fallback to serving static files
       return serve_static("public", req);
