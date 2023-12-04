80d99263f53db35c9d1ee6780d49a416fcf32a34
kuryart <kuryart1@gmail.com>
2023-12-04 23:17:11 +0000

    adjust Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,10 +8,10 @@ export function Hero({
   children: ReactNode;
 }) {
   return (
-    <section className="hero bg-base-200 px-16 sm:px-32 py-24 sm:rounded-3xl">
+    <section className="hero bg-base-200 px-16 2xs:px-4 xs:px-8 sm:px-32 py-24 sm:rounded-3xl">
       <div className="hero-content text-center">
-        <div className="flex flex-col gap-6">
-          <h1 className="text-6xl">{title}</h1>
+        <div className="flex flex-col gap-6 w-full">
+          <h1 className="text-4xl">{title}</h1>
           {children}
         </div>
       </div>
