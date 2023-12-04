cfbd07deed34277edb4fe1472c72d376a5a2aa4d
kuryart <kuryart1@gmail.com>
2023-12-04 23:10:23 +0000

    comment purgecss to avoid some unexpected errors

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,12 +3,26 @@ module.exports = {
     "postcss-import": {
       from: "src/css/main.css",
     },
-    "tailwindcss": {},
-    "autoprefixer": {},
-    "@fullhuman/postcss-purgecss": {
-      content: ["src/**/*.tsx"],
-    },
-    "cssnano": {
+    tailwindcss: {},
+    // "@fullhuman/postcss-purgecss": {
+    //   content: ["src/**/*.tsx"],
+    //   keyframes: true,
+    //   variables: true,
+    //   rejected: true,
+    //   rejectedCss: true,
+    //   safelist: [
+    //     /(^|\s)sm:\S+/,
+    //     /(^|\s)md:\S+/,
+    //     /(^|\s)lg:\S+/,
+    //     /(^|\s)xl:\S+/,
+    //     /(^|\s)2xl:\S+/,
+    //     /(^|\s)hover:\S+/,
+    //     /(^|\s)flex\S+/,
+    //     /(^|\s)grid\S+/,
+    //   ],
+    // },
+    autoprefixer: {},
+    cssnano: {
       preset: "default",
     },
   },
